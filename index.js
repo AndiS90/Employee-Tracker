fbd560e5f973e901317ecde9363a4ef86401fa48
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2021-10-31 20:33:13 -0500

    'uploading readme hw for better access'

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
-      user: 'root',
+      user:process.env.DB_USER,
       // MySQL password
-      password: '',
-      database: 'courses_db'
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_NAME
     },
     console.log(`Connected to the company_db database.`)
   );
